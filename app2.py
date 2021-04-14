e40ae29f1d4acf7105a53f712a080d6f84623271
Bob Ciminera <bobc@Bobs-MBP.fios-router.home>
2021-04-14 08:55:37 -0400

    update py

---
+++ app2.py
@@ -0,0 +1,36 @@
+# Add dependencies
+import datetime as dt
+import numpy as np
+# import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+# Set up database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Set up Flask
+app = Flask (__name__)
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
